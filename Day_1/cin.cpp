7ca8aa7a573cf39d2e25155116e35dcae23616d5
zz <mrnxdzgggh@gmail.com>
2025-05-15 19:02:17 +0800

    修复环境错误

---
+++ Day_1/cin.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int calcu_num;
+    cout << "请输入int类型数字，然后将会返回该值的2倍：" << endl;
+
+    // 尝试读取用户输入
+    if (!(cin >> calcu_num)) { // 检查输入是否有效
+        cout << "输入无效！请输入整数。\r\n" << endl;
+        cin.clear(); // 清除错误标志
+        cin.ignore(10000, '\n'); // 丢弃缓冲区中剩余的无效输入
+    } else {
+        cout << "返回值：" << calcu_num * 2 << endl;
+    }
+    system("pause");
+    return 0;
+}
--- Day_1/circle.cpp
+++ Day_1/circle.cpp
@@ -5,7 +5,10 @@ int main()
     int i = -65536;
     for (; i <= 65535; i++)
     {
-        cout << "i=" << i << endl;
+        if (i % 10000 == 0)
+        {
+            cout << "i=" << i << endl;
+        }
     }
     return 0;
 }
